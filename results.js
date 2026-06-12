b802e8436cd274ad8bb8d6a89a6fd46ef7f5e434
FabianB-beep <faabi@gmx.de>
2026-06-12 14:14:59 +0200

    Cornhole Ergebnisse & KO-Paarungen aktualisiert

--- results.js
+++ results.js
@@ -106,12 +106,8 @@ const CORNHOLE_PAIRINGS = {
 
 const CORNHOLE_RESULTS = {
   // Gruppenphase A
-  1: [1, 3],
   // Gruppenphase B
-  10: [7, 21],
   // Gruppenphase C
-  14: [1, 5],
-  17: [10, 22],
   // Halbfinale
   // Platz 3 & Finale
 };
